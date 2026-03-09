e39bfe287031785f117971affa5ffa4ff3690531
joão277 <joaobstarocha@gmail.com>
2026-03-09 18:07:42 -0300

    layered architecture

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(JSON.stringify({
+        message: "API Order running"
+    }));
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
